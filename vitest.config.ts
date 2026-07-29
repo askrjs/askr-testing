e16bf739555c29e04014176e429c96cd66f66b22
Jeff Repanich <jrepanich@gmail.com>
2026-07-29 17:25:33 -0400

    chore: commit outstanding local work (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: { include: ["tests/**/*.test.ts"] },
+  test: {
+    include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      reporter: ["text", "json-summary"],
+      thresholds: {
+        statements: 99,
+        branches: 88,
+        functions: 100,
+        lines: 100,
+        "src/cookies.ts": {
+          branches: 53,
+        },
+      },
+    },
+  },
 });
